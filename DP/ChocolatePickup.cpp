82d41afd8be6b6a4635d6592f1e17718dd5a8232
AthSat <atharvsathe28704@gmail.com>
2025-04-17 08:40:54 +0530

    Add tabulation implementation for Chocolate Pickup problem

--- DP/ChocolatePickup.cpp
+++ DP/ChocolatePickup.cpp
@@ -2,6 +2,24 @@
 #include <vector>
 using namespace std;
 
+/*
+Ninja has a 'GRID' of size 'R' X 'C'. Each cell of the grid contains some chocolates. 
+Ninja has two friends Alice and Bob, and he wants to collect as many chocolates as 
+possible with the help of his friends.
+
+Initially, Alice is in the top-left position i.e. (0, 0), and Bob is in the top-right 
+place i.e. (0, ‘C’ - 1) in the grid. Each of them can move from their current cell to 
+the cells just below them. When anyone passes from any cell, he will pick all chocolates 
+in it, and then the number of chocolates in that cell will become zero. If both stay in 
+the same cell, only one of them will pick the chocolates in it.
+
+If Alice or Bob is at (i, j) then they can move to (i + 1, j), (i + 1, j - 1) or 
+(i + 1, j + 1). They will always stay inside the ‘GRID’.
+
+Your task is to find the maximum number of chocolates Ninja can collect with the 
+help of his friends by following the above rules.
+*/
+
 int recurseNaive(vector<vector<int>>& grid, int row, int alC, int bobC) {
   int m = grid.size(), n = grid[0].size();
   if (bobC >= n || bobC < 0 || alC >= n || alC < 0) {
@@ -53,6 +71,42 @@ int memoization(vector<vector<int>>& grid, vector<vector<vector<int>>>& memo, in
   }
   memo[row][alC][bobC] = maxSum;
   return maxSum;
+}
+
+int tabulation(vector<vector<int>>& grid) {
+  int m = grid.size(), n = grid[0].size();
+  vector<vector<vector<int>>> tab(m, vector<vector<int>>(n, vector<int>(n, -1)));
+
+  for (int al = 0; al < n; al++) {
+      for (int bob = 0; bob < n; bob++) {
+          if (al == bob) tab[m - 1][al][bob] = grid[m - 1][al];
+          else tab[m - 1][al][bob] = grid[m - 1][al] + grid[m - 1][bob];
+      }
+  }
+
+  for (int row = m - 2; row >= 0; row--) {
+      for (int alC = 0; alC <= n - 1; alC++) {
+          for (int bobC = 0; bobC <= n - 1; bobC++) {
+              int maxSum = INT_MIN;
+              for (int i = -1; i <= 1; i++) {
+                  for (int j = -1; j <= 1; j++) {
+                      int j1 = alC + i, j2 = bobC + j;
+                      if (j1 < 0 || j1 >= n || j2 < 0 || j2 >= n) {
+                          continue;
+                      }
+                      if (alC == bobC) {
+                          maxSum = max(maxSum, grid[row][alC] + tab[row + 1][alC + i][bobC + j]);
+                      } else {
+                          maxSum = max(maxSum, grid[row][alC] + grid[row][bobC] + tab[row + 1][alC + i][bobC + j]);
+                      }
+                  }
+              }
+              tab[row][alC][bobC] = maxSum;
+          }
+      }
+  }
+
+  return tab[0][0][n - 1];
 }
 
 int maximumChocolates(int r, int c, vector<vector<int>> &grid) {
