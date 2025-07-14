ab0834374b7cf6fe3e0510114ba762c6775faee3
AthSat <atharvsathe28704@gmail.com>
2025-07-14 16:18:52 +0530

    Add implementation for calculating the maximum width of a binary tree

---
+++ Trees/MaxWidthBT.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+Given the root of a binary tree, return the maximum width of the given tree.
+
+The maximum width of a tree is the maximum width among all levels.
+
+The width of one level is defined as the length between the end-nodes (the
+leftmost and rightmost non-null nodes), where the null nodes between the
+end-nodes that would be present in a complete binary tree extending down to that
+level are also counted into the length calculation.
+
+It is guaranteed that the answer will in the range of a 32-bit signed integer.
+*/
+
+int widthOfBinaryTree(TreeNode* root) {
+  queue<pair<TreeNode*, unsigned long long>> q;
+  q.push({root, 0});
+
+  int maxWidth = 1;
+  while (!q.empty()) {
+    int levelSize = q.size();
+    unsigned long long leftMost = q.front().second;
+    unsigned long long rightMost = leftMost;
+
+    for (int i = 0; i < levelSize; i++) {
+      auto [node, pos] = q.front();
+      q.pop();
+
+      rightMost = pos;
+
+      if (node->left) {
+        q.push({node->left, 2 * pos});
+      }
+
+      if (node->right) {
+        q.push({node->right, 2 * pos + 1});
+      }
+    }
+
+    maxWidth = max(maxWidth, (int)(rightMost - leftMost + 1));
+  }
+  return maxWidth;
+}
+
+int main() { return 0; }
